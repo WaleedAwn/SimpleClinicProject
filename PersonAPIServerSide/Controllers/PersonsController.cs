1a641dbf6c8d3f1f3ba2d0c6d7df330e5aef83bc
LEGION <LEGION@W-A>
2024-09-12 19:34:03 +0300

    Add project files.

---
+++ PersonAPIServerSide/Controllers/PersonsController.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using PersonsAPIBusinessLayer;
+using PersonsAPIDataAccessLayer;
+
+
+namespace PersonAPIServerSide.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/Persons")]
+
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+
+        [HttpGet("All", Name = "GetAllPersons")]
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<PersonsDTO>> GetAllPersons()
+        {
+            var PersonList = PersonsAPIBusinessLayer.Persons.GetAllPersons();
+            if (PersonList.Count == 0)
+            {
+                return NotFound("No Persons Found");
+            }
+            return Ok(PersonList);
+
+        }
+
+        [HttpGet("{id}", Name = "GetPersonByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<PersonsDTO> GetPersonByID(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest("bad Request");
+            }
+
+            PersonsAPIBusinessLayer.Persons person = PersonsAPIBusinessLayer.Persons.Find(id);
+
+            if (person == null)
+            {
+                return NotFound("No Student found");
+            }
+
+            PersonsDTO sDTO = person.SDTO;
+
+
+            return Ok(sDTO);
+        }
+
+
+
+        [HttpPost(Name = "AddPerson")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<PersonsDTO> AddNewPerson( PersonsDTO newPersonDTO)
+        {
+            if (newPersonDTO == null || string.IsNullOrEmpty(newPersonDTO.PersonName)
+               || string.IsNullOrEmpty(newPersonDTO.Gender)
+               || string.IsNullOrEmpty(newPersonDTO.PhoneNumber) || string.IsNullOrEmpty(newPersonDTO.Email)
+               || string.IsNullOrEmpty(newPersonDTO.Address))
+            {
+                return BadRequest("Invalid Person data");
+            }
+
+            PersonsAPIBusinessLayer.Persons person = new PersonsAPIBusinessLayer.Persons(new PersonsDTO(newPersonDTO.Id,
+                newPersonDTO.PersonName, newPersonDTO.DateOfBirth, newPersonDTO.Gender, newPersonDTO.PhoneNumber
+                , newPersonDTO.Email, newPersonDTO.Address));
+
+            person.Save();
+            newPersonDTO.Id = person.PersonID;
+            return CreatedAtRoute("GetPersonByID", new { id = newPersonDTO.Id }, newPersonDTO);
+
+
+        }
+
+
+
+        [HttpPut("{id}", Name = "UpdatePerson")]
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<PersonsDTO> UpdatePerson(int id,  PersonsDTO UpdatePersonDTO)
+        {
+            if (id < 1 || UpdatePersonDTO == null || string.IsNullOrEmpty(UpdatePersonDTO.PersonName) || string.IsNullOrEmpty(UpdatePersonDTO.Gender)
+                 || string.IsNullOrEmpty(UpdatePersonDTO.PhoneNumber) || string.IsNullOrEmpty(UpdatePersonDTO.Email) || string.IsNullOrEmpty(UpdatePersonDTO.Address))
+            {
+                return BadRequest("Invalid Person data");
+            }
+            PersonsAPIBusinessLayer.Persons persons = PersonsAPIBusinessLayer.Persons.Find(id);
+
+
+            if (persons == null)
+            {
+                return NotFound("No Persons found");
+            }
+
+            persons.PersonID = UpdatePersonDTO.Id;
+            persons.PersonName = UpdatePersonDTO.PersonName;
+            persons.DateOfBirth = UpdatePersonDTO.DateOfBirth;
+            persons.Gender = UpdatePersonDTO.Gender;
+            persons.PhoneNumber = UpdatePersonDTO.PhoneNumber;
+            persons.Email = UpdatePersonDTO.Email;
+            persons.Address = UpdatePersonDTO.Address;
+
+            if (persons.Save())
+            {
+                //return the DTO not the Full Object
+                return Ok(persons.SDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { message = " Error Updating Person" });
+            }
+
+
+
+
+        }
+
+
+
+        [HttpDelete("{id}", Name = "DeletePerson")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+
+        public ActionResult DeletePerson(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest("bad Request");
+            }
+
+            
+            if (!Persons.IsPersonExists(id))
+            {
+                return NotFound($"Person with ID {id} not exist");
+            }
+
+            if (Persons.CheckPersonRelations(id))
+                return Conflict("Can't delete this Person because it has relations in other tables");
+
+
+            if (PersonsAPIBusinessLayer.Persons.DeletePerson(id))
+            {
+                return Ok($"Person with ID {id} has been deleted");
+            }
+            else
+            {
+                return StatusCode(500, new { message = $"Error deleting person with ID {id}" });
+            }
+
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
---
+++ PersonsAPIDataAccessLayer/PersonsData.cs
@@ -0,0 +1,269 @@
+﻿
+using System;
+using System.Data;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+namespace PersonsAPIDataAccessLayer
+{
+    public class PersonsDTO
+    {
+        public PersonsDTO(int id, string personName, DateTime dateOfBirth, string gender, string PhoneNumber, string email, string address)
+        {
+            this.Id = id;
+            this.PersonName = personName;
+            this.DateOfBirth = dateOfBirth;
+            this.Gender = gender;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = email;
+            this.Address = address;
+        }
+
+        public int Id { get; set; }
+        public string PersonName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+
+    public class PersonsData
+    {
+        private static string _connectionString = "Server=localhost;Database=SClinic;User Id=sa;Password=sa123456;TrustServerCertificate=True;";
+
+        public static List<PersonsDTO> GetAllPersons()
+        {
+            var PersonsList = new List<PersonsDTO>();
+
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_GetAllPersons", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            PersonsList.Add(new PersonsDTO
+                            (
+                                //(PersonID, Name, DateOfBirth, Gender, PhoneNumber, Email, Address
+                                reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                reader.GetString(reader.GetOrdinal("Name")),
+                                reader.GetDateTime(reader.GetOrdinal("DateOfBirth")),
+                                reader.GetString(reader.GetOrdinal("Gender")),
+                                reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                                reader.GetString(reader.GetOrdinal("Email")),
+                                reader.GetString(reader.GetOrdinal("Address"))
+
+                            ));
+                        }
+                    }
+                }
+                conn.Close();
+
+                return PersonsList;
+            }
+
+
+
+        }
+
+        public static PersonsDTO GetPersonByID(int PersonID)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_GetPersonByID", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PersonID", PersonID);
+
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new PersonsDTO
+                             (
+
+                                 reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                 reader.GetString(reader.GetOrdinal("Name")),
+                                 reader.GetDateTime(reader.GetOrdinal("DateOfBirth")), // Extract only the Date part
+                                 reader.GetString(reader.GetOrdinal("Gender")),
+                                 reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                                 reader.GetString(reader.GetOrdinal("Email")),
+                                 reader.GetString(reader.GetOrdinal("Address"))
+
+
+                             );
+                        }
+
+                        else { return null; }
+                    }
+                }
+
+
+
+            }
+
+
+
+        }
+
+        public static int AddNewPerson(PersonsDTO NewPersonDTOInfo)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("SP_AddNewPerson", connection))
+                {
+                    //(PersonID, Name, DateOfBirth, Gender, PhoneNumber, Email, Address
+                    command.CommandType = CommandType.StoredProcedure;
+                    //command.Parameters.AddWithValue("@PersonID", NewPersonDTOInfo.PersonID);
+                    command.Parameters.AddWithValue("@Name", NewPersonDTOInfo.PersonName);
+                    command.Parameters.AddWithValue("@DateOfBirth", NewPersonDTOInfo.DateOfBirth.Date);
+                    command.Parameters.AddWithValue("@Gender", NewPersonDTOInfo.Gender);
+                    command.Parameters.AddWithValue("@PhoneNumber", NewPersonDTOInfo.PhoneNumber);
+                    command.Parameters.AddWithValue("@Email", NewPersonDTOInfo.Email);
+                    command.Parameters.AddWithValue("@Address", NewPersonDTOInfo.Address);
+
+                    var outPutIdParm = new SqlParameter("@NewPersonId", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(outPutIdParm);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    return (int)outPutIdParm.Value;
+
+                }
+
+            }
+
+        }
+
+        public static bool UpdatePerson(PersonsDTO UpdatePersonDTOinfo)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("SP_UpdatePerson", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@PersonID", UpdatePersonDTOinfo.Id);
+                    command.Parameters.AddWithValue("@Name", UpdatePersonDTOinfo.PersonName);
+                    command.Parameters.AddWithValue("@DateOfBirth", UpdatePersonDTOinfo.DateOfBirth);
+                    command.Parameters.AddWithValue("@Gender", UpdatePersonDTOinfo.Gender);
+                    command.Parameters.AddWithValue("@PhoneNumber", UpdatePersonDTOinfo.PhoneNumber);
+                    command.Parameters.AddWithValue("@Email", UpdatePersonDTOinfo.Email);
+                    command.Parameters.AddWithValue("@Address", UpdatePersonDTOinfo.Address);
+
+                    var outPutIdParm = new SqlParameter("@RowsAffected", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(outPutIdParm);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    return (int)outPutIdParm.Value > 0;
+
+                }
+
+            }
+        }
+
+        public static bool DeletePerson(int PersonId)
+        {
+            int rowsAffected = 0;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("SP_DeletePerson", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@PersonID", PersonId);
+
+
+                    connection.Open();
+
+                    rowsAffected = (int)command.ExecuteScalar();
+                    return (rowsAffected == 1);
+                }
+
+            }
+
+
+           
+
+        }
+
+
+        public static bool IsPersonExist(int PersonId)
+        {
+            int isFound = 0;
+            bool isExist = false;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+
+                using (SqlCommand command = new SqlCommand("SP_CheckPersonExists", connection))
+                {
+                    command.CommandType= CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+
+                    SqlParameter returnParameter = new SqlParameter();
+                    returnParameter.Direction = ParameterDirection.ReturnValue;
+                    command.Parameters.Add(returnParameter);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    isFound = (int)returnParameter.Value;
+
+                    isExist =  isFound != 0;
+                }
+            }
+            return isExist;
+               
+        }
+
+        public static bool IsPersonHasRelation(int PersonId)
+        {
+            bool isFound = false;
+            int returnValue = 0;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+
+                using (SqlCommand command = new SqlCommand("SP_CheckPersonRelations", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+
+                    SqlParameter returnParameter = new SqlParameter();
+                    returnParameter.Direction = ParameterDirection.ReturnValue;
+                    command.Parameters.Add(returnParameter);
+                    
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    returnValue = (int)returnParameter.Value;
+                    isFound = returnValue != 0;
+                }
+            }
+            return isFound;
+
+        }
+
+
+
+    }
+}
+
+
+ 
