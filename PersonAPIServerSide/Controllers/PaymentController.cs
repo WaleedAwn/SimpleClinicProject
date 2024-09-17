e5bdf2ce4c2a47385c2574414e372e79357fe5b8
A-KAlqadsi <abdulkareemalqa@gmail.com>
2024-09-17 06:37:35 +0300

    preparing the payment folders and classes

---
+++ PersonAPIServerSide/Controllers/PaymentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonAPIServerSide.Controllers
+{
+    [Route("api/[controller]")]
+    [Route("api/PaymentApi")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+
+    }
+}
